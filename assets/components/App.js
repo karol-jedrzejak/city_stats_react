20448da4cdd10789fac4a75cf680e8c25be97736
karol-jedrzejak <karol.jedrzejak@gmail.com>
2024-10-07 14:36:48 +0200

    up
    up

---
+++ assets/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navi from "./navi";
+import Navi2 from "./Navi2";
+
+const App = () => {
+  return (
+    <div>
+      <Navi2 />
+      <Navi />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Controller/TestController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', methods: ['GET'], name: 'app_test.index')]
+    public function index(): Response
+    {
+        return $this->render('test.html.twig');
+    }
+
+    #[Route('/api/test', methods: ['GET'], name: 'app_test2.index')]
+    public function getUsers(): Response
+    {
+        $users = [
+            [
+                'id' => 1,
+                'name' => 'Olususi Oluyemi22',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Camila Terry',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/men/42.jpg'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Joel Williamson',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/67.jpg'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Deann Payne',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+            ],
+            [
+                'id' => 5,
+                'name' => 'Donald Perkins',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+                'imageURL' => 'https://randomuser.me/api/portraits/men/89.jpg'
+            ]
+        ];
+
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setContent(json_encode($users));
+
+        return $response;
+    }
+
+    #[Route('/api/testb', methods: ['GET'], name: 'app_test3.index')]
+    public function getUsers2(): Response
+    {
+        $users = [
+            [
+                'id' => 1,
+                'name' => 'aa',
+                'description' => 'xx',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/50.jpg'
+            ],
+            [
+                'id' => 2,
+                'name' => 'bb',
+                'description' => 'bb',
+                'imageURL' => 'https://randomuser.me/api/portraits/men/42.jpg'
+            ],
+            [
+                'id' => 3,
+                'name' => 'cc',
+                'description' => 'cc',
+                'imageURL' => 'https://randomuser.me/api/portraits/women/67.jpg'
+            ]
+        ];
+
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setContent(json_encode($users));
+
+        return $response;
+    }
+}
